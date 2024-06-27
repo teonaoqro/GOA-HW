ea1cb508f4cc7f7119e4e1d470bf5062a9b94857
teonaoqro <teonaoqropilashvili8@gmail.com>
2024-06-27 15:13:53 +0400

    commit

--- day 01/homework/lesson 1.py
+++ day 01/homework/lesson 1.py
@@ -1,32 +1,8 @@
-name ="teona"
-#name არის ცვლადი
-# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
-# "teona"არის ცვლადისთვის მნიშვნელობა
+name=" hello, i am teona,"
+last_name="my last_name  is oqropilashvili."
+age= "my age is 31 years old."
+height=" my height is 170 sm."
+country= "i live in  georgia"
+hobby= " and my hobby is beeng alone."
 
-print(name)
-surname = "oqropilashvili"
-#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
-
-
-name = "teona" #ეს არის str(string)ტიპის ცვლადი
-age =31 #ეს არის int (integer)მთელი რიცხვი
-height = 170.0 #ეს არის float ტიპის ცვლადი(ათწილადი)
-#boolean (bool) ტიპის ცვლადი
-
-knows_programming = True #true ან falce
-is_ugly = False  #snakecase(უბრალოდ წერის სტილი სტანდარტულად)
-
-isUgly = False # ჯავასკრიპტული camelcase
-
-print(name + " " + surname)
-#STRING არის ბრჭყალებში მოქცეული სიმბოლო
-#PRINT(name + age)
-
-#print(type(age))
-#print(type(name))
-#print(type(surname))
-#print(type(height))
-#print(type(knows_programming))
-
-print(name + " " + str(age))
-print(name + " " + surname + " " + str(age) + " " + str(height))
+print(name + " " + last_name + " " + age + " " + height + " " + country + " " + hobby)
