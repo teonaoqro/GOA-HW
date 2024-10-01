3c00667f59116186ae0e0eec403581857ab9f50b
teonaoqro <teonaoqropilashvili8@gmail.com>
2024-10-02 00:08:53 +0400

    commit

---
+++ day 29/homework/hw27.py
@@ -0,0 +1,6 @@
+#27)შექმენი სია, რომელიც შეიცავს სტრინგებს. გამოიყენე len() თითოეულ სტრინგზე, რათა გაიგო მათი სიგრძეები ცალ-ცალკე.
+string=["teona","oqropilashvili"]     
+string1=string[0]
+string2=string[1]
+print(len(string1))
+print(len(string2))
