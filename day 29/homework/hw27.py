6441e92b763fe94a06cec0a69714edb9a76afd47
teonaoqro <teonaoqropilashvili8@gmail.com>
2024-10-12 00:20:52 +0400

    commit

--- day 29/homework/hw27.py
+++ day 29/homework/hw27.py
@@ -1,6 +1,3 @@
 #27)შექმენი სია, რომელიც შეიცავს სტრინგებს. გამოიყენე len() თითოეულ სტრინგზე, რათა გაიგო მათი სიგრძეები ცალ-ცალკე.
 string=["teona","oqropilashvili"]     
-string1=string[0]
-string2=string[1]
-print(len(string1))
-print(len(string2))
+for i in range(len(string))
